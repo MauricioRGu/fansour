7f00ed28f36b5d3360ca944d0223416107196236
Mauricio Guimaraes <mauricio.guimaraes.r@gmail.com>
2023-12-21 07:45:27 -0300

    sicroniza

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   #imagens de perfil e capa
   has_one_attached :avatar
   has_one_attached :capa
+  #imagens de validação do perfil criador de conteudo
+  has_many_attached :validate_creator
 
   #cria nome de usuario antes de criar perfil
   before_create :username
